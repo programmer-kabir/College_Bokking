27c3eb3035e84e71250e7b94a20df2a82e5f7e34
programmer-kabir <programmerkabirr.com>
2024-12-08 12:56:43 +0600

    popular college data added

--- Client_Side/src/Pages/Home/Home.jsx
+++ Client_Side/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../Components/HomePages/Banner/Banner'
+import PopularCollege from '../../Components/HomePages/PopularCollege/PopularCollege'
 
 const Home = () => {
   return (
-    <div>
+    <div className='space-y-9'>
       <Banner />
+      <PopularCollege />
     </div>
   )
 }
